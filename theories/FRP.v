c7c7c46075dbf78e7d6d0592393750be89e09b57
soukouki <soukouki0@yahoo.co.jp>
2025-04-16 14:16:36 +0900

    occsとstepsを持ってきた

--- theories/FRP.v
+++ theories/FRP.v
@@ -1,4 +1,121 @@
-Require CoqFRP.Test.
-Export Test.
-Print test.
+
+Set Implicit Arguments.
+
+From Stdlib Require Import ssreflect.
+From Stdlib Require Import List Recdef Nat.
+Import ListNotations.
+Import PeanoNat.Nat.
+
+(* splitプリミティブを実装しないため、論理的時刻はnatで十分 *)
+Definition time := nat.
+
+Definition str a := list (time * a).
+Definition cel a := (a * (list (time * a)))%type.
+
+Fixpoint str_timing_is_asc_order a (s : str a) : bool :=
+  match s with
+  | nil => true
+  | (t1, a1) :: nil => true
+  | (t1, a1) :: ((t2, a2) :: _) as tas => (t1 <? t2) && str_timing_is_asc_order tas
+  end.
+
+Inductive stream a :=
+  | mk_stream : { s : str a | str_timing_is_asc_order s = true } -> stream a
+  | never : stream a
+  | map_s prev : (prev -> a) -> stream prev -> stream a
+  | snapshot prev_s prev_c : (prev_s -> prev_c -> a) -> stream prev_s -> cell prev_c -> stream a
+  | merge : stream a -> stream a -> (a -> a -> a) -> stream a
+  | filter : (a -> bool) -> stream a -> stream a
+with cell a :=
+  | constant : a -> cell a
+  | hold : a -> stream a -> time -> cell a
+  | map_c prev : (prev -> a) -> cell prev -> cell a
+  | apply prev : cell (prev -> a) -> cell prev -> cell a.
+
+Definition at_ a (cel : cel a) (t : time) : a :=
+  last (map snd (List.filter (fun ta => (fst ta) <? t) (snd cel))) (fst cel).
+
+(* リストを前から見ていき、前2つが同じ時刻であれば、関数fでまとめたリストを返す *)
+Function coalesce a (f : a -> a -> a) (s : str a) {measure length s} : str a :=
+  match s with
+  | (t1, a1) :: ((t2, a2) :: tas2) as tas1 =>
+    if t1 =? t2
+    then
+      coalesce f ((t1, f a1 a2) :: tas2)
+    else
+      (t1, a1) :: coalesce f tas1
+  | ta1 :: tas => ta1 :: coalesce f tas
+  | [] => []
+  end.
+Proof.
+- by auto.
+- by auto.
+- by auto.
+Qed.
+
+Function occs_knit a (ab : (str a * str a)) {measure (fun (ab : str a * str a) => length (fst ab) + length (snd ab)) ab} :=
+  match ab with
+  | (((ta, a) :: tas2) as tas1,  ((tb, b) :: tbs2) as tbs1) =>
+    if ta <=? tb
+    then
+      (ta, a) :: occs_knit (tas2, tbs1)
+    else
+      (tb, b) :: occs_knit (tas1, tbs2)
+  | (tas, tbs) => tas ++ tbs
+  end.
+Proof.
+- by auto.
+- move => a ab tas tbs p tas2 ta a0 H1 H2 p0 tbs2 tb b H3 H4 H5 H6.
+  rewrite /=.
+  rewrite -succ_lt_mono.
+  by apply add_lt_mono_l.
+Qed.
+
+Function steps_knit p a (f0 : p -> a) (p0 : p) (fps : (list (time * (p -> a)) * list (time * p)))
+  {measure (fun fps => length (fst fps) + length (snd fps)) fps}
+  : list (time * a) :=
+  match fps with
+  | (((ft1, f1) :: fs1) as fs, ((pt1, p1) :: ps1) as ps) =>
+    match ft1 ?= pt1 with
+    | Lt => steps_knit f1 p0 (fs1, ps)
+    | Gt => steps_knit f0 p1 (fs, ps1)
+    | Eq => steps_knit f1 p1 (fs1, ps1)
+    end
+  | ((ft1, f1) :: fs1, []) => steps_knit f1 p0 (fs1, [])
+  | ([], (pt1, p1) :: ps1) => steps_knit f0 p1 ([], ps1)
+  | ([], []) => []
+  end.
+Proof.
+- by auto.
+- by auto.
+- move => p a f0 p0 fps fs ps ft1f1 fs1 ft1 f1 H1 H2 pt1p1 ps1 pt1 p1 H3 H4 H5 H6.
+  rewrite /= add_succ_r.
+  by apply le_S.
+- by auto.
+- move => p a f0 p0 fps fs ps ft1f1 fs1 ft1 f1 H1 H2 pt1p1 ps1 pt1 p1 H3 H4 H5 H6.
+  by rewrite /= add_succ_r.
+Qed.
+
+Fixpoint occs a (s_ : stream a) : str a :=
+  match s_ with
+  | mk_stream s => proj1_sig s
+  | never _ => []
+  | map_s f s => map (fun ta => (fst ta, f (snd ta))) (occs s)
+  | snapshot f s c => map (fun ta => (fst ta, f (snd ta) (at_ (steps c) (fst ta)))) (occs s)
+  | merge sa sb f => coalesce f (occs_knit (occs sa, occs sb))
+  | filter pred s => List.filter (fun ta => pred (snd ta)) (occs s)
+  end
+with steps a (c_ : cell a) : cel a :=
+  match c_ with
+  | constant a => (a, [])
+  | hold a s t0 => (a, coalesce (fun x y => y) (List.filter (fun ta => t0 <? fst ta) (occs s)))
+  | map_c f c =>
+    let stp := steps c in
+    (f (fst stp), map (fun ta => (fst ta, f (snd ta))) (snd stp))
+  | apply cf cp =>
+    let (f, fsts) := steps cf in
+    let (p, psts) := steps cp in
+    (f p, steps_knit f p (fsts, psts))
+  end.
+
 
