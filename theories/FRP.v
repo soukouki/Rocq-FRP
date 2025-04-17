8f354909a34c421ec371ac0458e195dfa17c7e7d
soukouki <soukouki0@yahoo.co.jp>
2025-04-17 16:10:45 +0900

    あーーーーー、分からん

--- theories/FRP.v
+++ theories/FRP.v
@@ -118,4 +118,185 @@ with steps a (c_ : cell a) : cel a :=
     (f p, steps_knit f p (fsts, psts))
   end.
 
+Lemma str_timing_is_asc_order_tail a (tp1 : time * a) tps1 :
+  str_timing_is_asc_order (tp1 :: tps1) = true -> str_timing_is_asc_order tps1 = true.
+Proof.
+case tp1 => t1 a1.
+case tps1 => // tp2 tps2; clear tps1.
+case tp2 => t2 a2.
+rewrite /= => H1.
+rewrite Bool.andb_true_iff in H1.
+by case H1.
+Qed.
+
+Lemma str_timing_is_asc_order_head a t0 a0 (s0 : str a) :
+  str_timing_is_asc_order s0 = true ->
+  (forall t' a', In (t', a') s0 -> t0 < t') ->
+  str_timing_is_asc_order ((t0, a0) :: s0) = true.
+Proof.
+move => H1 H2.
+rewrite /=.
+rewrite H1.
+move : H2.
+case s0 => // [[t1 a1] s1] H3.
+rewrite Bool.andb_true_r.
+rewrite ltb_lt.
+apply H3 with (a' := a1).
+by apply in_eq.
+Qed.
+
+(* 2時間位格闘したけれど示せなかった *)
+Lemma str_timing_is_asc_order_gt a t0 a0 (s0 : str a) :
+  str_timing_is_asc_order ((t0, a0) :: s0) = true ->
+  forall t' a', In (t', a') s0 -> t0 < t'.
+Proof.
+Admitted.
+
+Lemma str_timing_is_asc_order_first_gt a (ta1 ta2 : time * a) tas2 :
+  str_timing_is_asc_order (ta1 :: ta2 :: tas2) = true -> fst ta1 <? fst ta2 = true.
+Proof.
+case ta1 => t1 a1.
+case ta2 => t2 a2.
+rewrite /= => H1.
+rewrite Bool.andb_true_iff in H1.
+by case H1.
+Qed.
+
+Lemma str_timing_is_asc_order_iff a t0 a0 (s0 : str a) :
+  str_timing_is_asc_order s0 = true /\ (forall t' a', In (t', a') s0 -> t0 < t') <->
+  str_timing_is_asc_order ((t0, a0) :: s0) = true.
+Proof.
+split => [ [H1 H2] | H1 ].
+- apply str_timing_is_asc_order_head => // t'.
+- split.
+  + by apply str_timing_is_asc_order_tail in H1.
+  + by apply str_timing_is_asc_order_gt with (a0 := a0).
+Qed.
+
+Lemma str_timing_is_asc_order_map_s p a (f : p -> a) (s : stream p) :
+  str_timing_is_asc_order (occs s) = true -> str_timing_is_asc_order (occs (map_s f s)) = true.
+Proof.
+rewrite /=.
+move : (occs s) => tas0 H1.
+induction tas0 as [ | ta1 tas1 IH ] => //=.
+rewrite IH; clear IH.
+- by apply str_timing_is_asc_order_tail in H1.
+- move : tas1 H1.
+  case => // ta2 tas2 H1.
+  rewrite /=.
+  rewrite Bool.andb_true_iff.
+  split => //.
+  by apply str_timing_is_asc_order_first_gt in H1.
+Qed.
+
+Lemma str_timing_is_asc_order_snapshot a prev_s prev_c (f : prev_s -> prev_c -> a) (s : stream prev_s) (c : cell prev_c) :
+  str_timing_is_asc_order (occs s) = true -> str_timing_is_asc_order (occs (snapshot f s c)) = true.
+Proof.
+rewrite /=.
+move : (occs s) => tas0 H1.
+induction tas0 as [ | ta1 tas1 IH ] => //=.
+rewrite IH; clear IH.
+- by apply str_timing_is_asc_order_tail in H1.
+- move: tas1 H1.
+  case => // ta2 tas2 H1.
+  rewrite /=.
+  rewrite Bool.andb_true_iff.
+  split => //.
+  by apply str_timing_is_asc_order_first_gt in H1.
+Qed.
+
+Lemma str_timing_is_asc_order_coalesce_ignore_head_value a (s0 : str a) (f : a -> a -> a) t0 a0 b0 :
+  str_timing_is_asc_order (coalesce f ((t0, a0) :: s0)) = true ->
+  str_timing_is_asc_order (coalesce f ((t0, b0) :: s0)) = true.
+Proof.
+move : t0 a0 b0.
+induction s0 as [ | [t1 a1] s1] => [ t0 a0 b0 | t0 a0 b0 ].
+  by rewrite 4!coalesce_equation.
+rewrite coalesce_equation [coalesce f ((t0, b0) :: _ )]coalesce_equation.
+case_eq (t0 =? t1) => H1.
+- rewrite eqb_eq in H1.
+  by apply IHs1.
+- rewrite eqb_neq in H1. (* いらんかも *)
+  Search str_timing_is_asc_order.
+  move => H2.
+  apply str_timing_is_asc_order_tail in H2.
+  apply str_timing_is_asc_order_head => // t' H3.
+  
+Admitted.
+
+Lemma str_timing_is_asc_order_coalesce a (s : str a) (f : a -> a -> a) :
+  str_timing_is_asc_order s = true -> str_timing_is_asc_order (coalesce f s) = true.
+Proof.
+induction s as [ | ta0 ] => [ _ | ].
+  by rewrite coalesce_equation.
+rewrite coalesce_equation.
+case ta0 => t0 a0.
+move : IHs.
+case s => [ IH _ | ta1 s1 ].
+  by rewrite coalesce_equation.
+case ta1 => t1 a1 => IH H1.
+case_eq (t0 =? t1) => H2.
+- apply str_timing_is_asc_order_tail in H1.
+  apply IH in H1.
+  rewrite eqb_eq in H2.
+  subst.
+  clear ta0 s ta1 IH.
+  move : H1.
+
+Admitted.
+
+Lemma str_timing_is_asc_order_merge a (s1 s2 : stream a) (f : a -> a -> a) :
+  str_timing_is_asc_order (occs s1) = true ->
+  str_timing_is_asc_order (occs s2) = true ->
+  str_timing_is_asc_order (occs (merge s1 s2 f)) = true.
+Proof.
+rewrite /=.
+move : (occs s1) (occs s2) => tas1 tas2.
+induction tas1 as [ | ] => [ _ H1 | H2 H3 ].
+- rewrite occs_knit_equation /=.
+  by apply str_timing_is_asc_order_coalesce.
+- rewrite occs_knit_equation.
+
+Admitted.
+
+Lemma str_timing_is_asc_order_filter a (f : a -> bool) (s : stream a) :
+  str_timing_is_asc_order (occs s) = true -> str_timing_is_asc_order (occs (filter f s)) = true.
+Proof.
+Admitted.
+
+Theorem str_timing_is_asc_order_occs a (s : stream a) :
+  str_timing_is_asc_order (occs s) = true.
+Proof.
+induction s.
+- rewrite /=.
+  by apply proj2_sig.
+- by [].
+- by apply str_timing_is_asc_order_map_s.
+- by apply str_timing_is_asc_order_snapshot.
+- by apply str_timing_is_asc_order_merge.
+- by apply str_timing_is_asc_order_filter.
+Qed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
